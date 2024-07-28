ffcaad5dbac95ecc1b173bb168ee3af99404f9ce
dizzy1978 <dizzy1978@gmail.com>
2024-07-28 22:54:52 +0300

    Update locators

--- PAGES/locators.py
+++ PAGES/locators.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
 class BasePageLocators:
     SEARCH_AREA = (By.XPATH, '//*[@placeholder="Фильмы, сериалы, персоны"]')
 
@@ -10,17 +9,17 @@ class MainPageLocators:
     SUBMIT_BUTTON = (By.XPATH, '//*[@type="submit"]')
 
 class SearchResultsPageLocators:
-    ITS_SEARCH_RESULT_PAGE = (By.XPATH, "//p[contains(text(),'Скорее всего, вы ищете')]")
-    SEARCH_TITLE = (By.XPATH, "//a[contains(text(),'Ирония судьбы') and contains(text(),'С легким паром')]")
-    # SEARH_RELEASE
+    SEARCH_RESULT_PAGE_MARK = (By.XPATH, "//div[@class='search_results']")  # Должен содержать "Скорее всего, вы ищете"
+    SEARCH_TITLE = (By.XPATH, "//a[contains(text(),'Ирония судьбы') and contains(text(),'С легким паром')]")  # for click
+    SEARCH_RATING = (By.XPATH, "//div[@class='element most_wanted']/div[@class='right']/div[@class='rating  ratingGreenBG']")  # bad bad bad
+    SEARCH_RELEASE = (By.XPATH, "//div[@class='element most_wanted']//span[@class='year']")  # so-so
 
 
-#
-# class FilmPageLocators:
+class FilmPageLocators:
+    FILM_TITLE =
+    FILM_RATING =
+    FILM_RELEASE =
 
 
 
 
-
-
-# Локатор для всех 26 картинок: [class="styles_link__HN8dS"]
--- main.py
+++ main.py
@@ -16,10 +16,18 @@ submit_button = browser.find_element(By.XPATH, '//button[@type="submit"]')
 time.sleep(5)
 submit_button.click()
 time.sleep(5)
-SEARCH_TITLE = browser.find_element(By.XPATH, "//a[contains(text(),'Ирония судьбы') and contains(text(),'С легким паром')]")
-time.sleep(5)
-SEARCH_TITLE.click()
-time.sleep(15)
+ITS_SEARCH_RESULT_PAGE = browser.find_element(By.XPATH, "//p[contains(text(),'Скорее всего, вы ищете')]")
+
+
+
+
+
+
+
+# SEARCH_TITLE = browser.find_element(By.XPATH, "//a[contains(text(),'Ирония судьбы') and contains(text(),'С легким паром')]")
+# time.sleep(5)
+# SEARCH_TITLE.click()
+# time.sleep(15)
 
 
 
