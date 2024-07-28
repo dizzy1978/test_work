8968bc7ecc67c6ea23601782c9fe79ee6f829fe3
dizzy1978 <dizzy1978@gmail.com>
2024-07-28 22:56:06 +0300

    Update locators

--- PAGES/locators.py
+++ PAGES/locators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class BasePageLocators:
     SEARCH_AREA = (By.XPATH, '//*[@placeholder="Фильмы, сериалы, персоны"]')
 
@@ -8,18 +9,18 @@ class MainPageLocators:
     SEARCH_AREA = (By.XPATH, '//*[@placeholder="Фильмы, сериалы, персоны"]')
     SUBMIT_BUTTON = (By.XPATH, '//*[@type="submit"]')
 
+
 class SearchResultsPageLocators:
     SEARCH_RESULT_PAGE_MARK = (By.XPATH, "//div[@class='search_results']")  # Должен содержать "Скорее всего, вы ищете"
-    SEARCH_TITLE = (By.XPATH, "//a[contains(text(),'Ирония судьбы') and contains(text(),'С легким паром')]")  # for click
-    SEARCH_RATING = (By.XPATH, "//div[@class='element most_wanted']/div[@class='right']/div[@class='rating  ratingGreenBG']")  # bad bad bad
+    SEARCH_TITLE = (
+    By.XPATH, "//a[contains(text(),'Ирония судьбы') and contains(text(),'С легким паром')]")  # for click
+    SEARCH_RATING = (By.XPATH,
+                     "//div[@class='element most_wanted']/div[@class='right']/div[@class='rating  ratingGreenBG']")  # bad bad bad
     SEARCH_RELEASE = (By.XPATH, "//div[@class='element most_wanted']//span[@class='year']")  # so-so
 
 
 class FilmPageLocators:
-    FILM_TITLE =
-    FILM_RATING =
-    FILM_RELEASE =
 
-
-
-
+# FILM_TITLE =
+# FILM_RATING =
+# FILM_RELEASE =
