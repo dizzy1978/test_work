23ac5cf5ceed0f8b0b45e7a9e30d5a416ca3c08c
dizzy1978 <dizzy1978@gmail.com>
2024-07-29 19:32:01 +0300

    PageObject pages methods update

--- PAGES/base_page.py
+++ PAGES/base_page.py
@@ -8,9 +8,10 @@ from .locators import BasePageLocators
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=15):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
