9e61ef5e0e3399d12d1c8db390e38f522edbbc50
dizzy1978 <dizzy1978@gmail.com>
2024-07-29 17:23:57 +0300

    Начало сборки модели PageObject

---
+++ PAGES/base_page.py
@@ -0,0 +1,35 @@
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
+
+
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PAGES/main_page.py
+++ PAGES/main_page.py
@@ -1,2 +1,15 @@
-# MAIN_PAGE:
-# https://www.kinopoisk.ru
+from .base_page import BasePage
+from .locators import MainPageLocators
+
+
+class MainPage(BasePage):
+
+    def should_be_search_area(self): # Проверка наличия поля поиска
+        assert self.is_element_present(*MainPageLocators.SEARCH_AREA), "Search area is not presented!"
+
+    def do_film_search(self, film_title): # Передаем название фильма в поиск и ждем "Найти"
+        search_area = self.browser.find_element(*MainPageLocators.SEARCH_AREA)
+        search_area.send_keys(film_title)
+        submit_button = self.browser.find_element(*MainPageLocators.SUBMIT_BUTTON)
+        submit_button.click()
+
