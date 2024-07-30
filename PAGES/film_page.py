9a0825152462ce407b7281ac8057974da381a084
dizzy1978 <dizzy1978@gmail.com>
2024-07-30 21:25:02 +0300

    test_all_suite update

--- PAGES/film_page.py
+++ PAGES/film_page.py
@@ -25,30 +25,30 @@ class FilmPage(BasePage):
     #     assert search_rating == film_rating, "Wrong film Rating!"
     #     print("---Film Rating is OK")
 
-    # def check_film_release_year(self):  # Проверка года выпуска фильма в карточке
+    # def check_film_release_year(self):  # Проверка года выпуска фильма в карточке!!!!!!!!!! Разные страницы
     #     assert SearchResultsPageLocators.SEARCH_RELEASE == FilmPageLocators.FILM_RELEASE, "Wrong film release Year!"
     #     print("---Film Release year is OK")
-    #
-    # def do_save_film_poster(self):  # Сохраняем постер фильма (путь захардкожен, можно вынести в локаторы/генерить. Линукс. os.makedirs(dir). )
-    #     poster_src = self.browser.find_element(FilmPageLocators.POSTER_SRC)
-    #     img_src = poster_src.get_attribute("src")
-    #     img = requests.get(img_src)
-    #     with open(r"film_data\main_image\poster.jpg", "wb") as file:
-    #         file.write(img.content)
-    #     print("---Film Poster image successfully saved")
-    #
-    # def do_save_film_info_to_json(self):  # Сохраняем инфо из блока "О фильме" в JSON файл  (путь захардкожен, лучше создавать. Слэши под Линукс)
-    #     about_film_block = self.browser.find_element(FilmPageLocators.ABOUT_FILM_BLOCK)  # Общий div блока
-    #     json_data = {}
-    #     for e in about_film_block.find_elements(FilmPageLocators.ALL_CHILD):
-    #         property = e.find_elements(FilmPageLocators.ALL_CHILD)
-    #         key = property[0].text
-    #         vals = property[1].find_elements(FilmPageLocators.ALL_CHILD)
-    #         valslist = [v.text for v in vals]
-    #         json_data[key] = valslist
-    #     with open(r'film_data\info\film_data.json', 'w') as f:
-    #         json.dump(json_data, f, ensure_ascii=False)
-    #     print("---Film data JSON-file successfully saved")
+
+    def do_save_film_poster(self):  # Сохраняем постер фильма (путь захардкожен, можно вынести в локаторы/генерить. Линукс. os.makedirs(dir). )
+        poster_src = self.browser.find_element(*FilmPageLocators.POSTER_SRC)
+        img_src = poster_src.get_attribute("src")
+        img = requests.get(img_src)
+        with open(r"film_data\main_image\poster.jpg", "wb") as file:
+            file.write(img.content)
+        print("---Film Poster image successfully saved")
+
+    def do_save_film_info_to_json(self):  # Сохраняем инфо из блока "О фильме" в JSON файл  (путь захардкожен, лучше создавать. Слэши под Линукс)
+        about_film_block = self.browser.find_element(*FilmPageLocators.ABOUT_FILM_BLOCK)  # Общий div блока
+        json_data = {}
+        for e in about_film_block.find_elements(*FilmPageLocators.ALL_CHILD):
+            property = e.find_elements(*FilmPageLocators.ALL_CHILD)
+            key = property[0].text
+            vals = property[1].find_elements(*FilmPageLocators.ALL_CHILD)
+            valslist = [v.text for v in vals]
+            json_data[key] = valslist
+        with open(r'film_data\info\film_data.json', 'w') as f:
+            json.dump(json_data, f, ensure_ascii=False)
+        print("---Film data JSON-file successfully saved")
 
     # def go_to_images_inset(self):  # Переходим на вкладку "Изображения"
     #     images_link = self.browser.find_element(FilmPageLocators.IMAGES_LINK)
--- test_all_suite.py
+++ test_all_suite.py
@@ -8,34 +8,40 @@ base_link = "https://www.kinopoisk.ru/"
 film_link = "https://www.kinopoisk.ru/film/77331/"
 
 # def test_do_search_film(browser):
-#     page = MainPage(browser, base_link)
-#     page.open()
-#     page.do_film_search()
+#     page = MainPage(browser, base_link)  # Working
+#     page.open()  # Working
+#     page.do_film_search()  # Working
 #     #page.should_be_search_results_page()
 #     #page.do_click_on_founded_film()
 
 
-# def test_founded_film_page(browser):
+# def test_founded_film_page(browser):  # Working
 #     page = FilmPage(browser, film_link)
 #     page.open()
 #     time.sleep(15)
 #     page.should_be_film_page()
 
 
-def test_check_film_title(browser):
-    page = FilmPage(browser, film_link)
-    page.open()
-    time.sleep(15)
-    page.check_film_title()
-
+# def test_check_film_title(browser):  # Working
+#     page = FilmPage(browser, film_link)
+#     page.open()
+#     time.sleep(15)
+#     page.check_film_title()
 
 
 # def test_check_film_rating(browser):
 #     pass
-#
-#
-#
+
 # def test_check_film_release_year(browser):
 #     pass
 
 
+# def test_do_save_film_poster(browser):  # Working
+#     page = FilmPage(browser, film_link)
+#     page.open()
+#     page.do_save_film_poster()
+
+def test_do_save_film_info_to_JSON(browser):
+    page = FilmPage(browser, film_link)
+    page.open()
+    page.do_save_film_info_to_json()
