6e70c4474d939595d857e13005b1ffb75c1fbe29
dizzy1978 <dizzy1978@gmail.com>
2024-07-31 13:42:33 +0300

    test_all_suite update

---
+++ z_save_fullscreen.py
@@ -0,0 +1,21 @@
+# Сохраняем full-screen окно браузера (захват по <body>)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+browser = webdriver.Chrome()
+
+url = "https://www.kinopoisk.ru/film/77331/"
+path_to_save = r"film_data\other_images\fullscreen.png"
+
+browser.get(url)
+browser.maximize_window()
+time.sleep(15)
+
+fs = browser.find_element(By.TAG_NAME, "body")
+png = fs.screenshot_as_png
+with open(path_to_save, "wb") as file:
+    file.write(png)
+print("---Fullscreen screenshot film Page successfully saved")
